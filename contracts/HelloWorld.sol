0a7e2b5dd535208cab62aa1fcb702ed69ef01727
Wenhui Feng <whfeng@CNwhfeng.local>
2018-05-08 16:18:44 +0800

    Add some contract

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.0;
+contract HelloWorld
+{
+    address creator;
+    string greeting;
+
+    constructor(string _greeting) public
+    {
+        creator = msg.sender;
+        greeting = _greeting;
+    }
+
+    function greet() constant public returns (string)
+    {
+        return greeting;
+    }
+
+    function setGreeting(string _newgreeting) public
+    {
+        greeting = _newgreeting;
+    }
+
+     /**********
+     Standard kill() function to recover funds
+     **********/
+
+    function kill() public
+    {
+        if (msg.sender == creator)
+            selfdestruct(creator);  // kills this contract and sends remaining funds back to creator
+    }
+}
---
+++ contracts/hello.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.4;
+ contract hello {
+  function sayHello() returns (string) {
+   return ("Hello World");
+ }
+}
